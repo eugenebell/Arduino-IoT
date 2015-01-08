c2adc0033b765da48558a947745d9d8c21844351
EugeneBell <eugene_bell@yahoo.com>
2015-01-08 11:47:40 +0000

    Updated and cleaned up code for Wifi connection and MQTT

--- SensorReader/SensorReader.ino
+++ SensorReader/SensorReader.ino
@@ -2,15 +2,8 @@
 #include <WiFiClient.h>
 #include <WiFiServer.h>
 #include <WiFiUdp.h>
-
 #include <PubSubClient.h>
-
-
 #include <SPI.h>
-
-/*
-
- */
 
 // named constant for the pin the sensor is connected to
 const int sensorPin = A0;
@@ -21,17 +14,14 @@ char ssid[] = "NETGEAR67";     //  your network SSID (name)
 char pass[] = "huskywater261";    // your network password
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
-//MQTT
-PubSubClient client;
 //#define MQTT_SERVER "192.168.0.8"
-byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+byte mac[]    = {  0x90, 0xA2, 0xDA, 0x0E, 0x07, 0x18 };
 byte serverRabbit[] = { 192, 168, 0, 4 };//192.168.0.8
-char server[] = "www.google.com";
 byte ip[]     = { 192, 168, 0, 4 };
 char* user = "guest";
 char* pwd = "guest";
 
-char topicTempHum[] = "hello/world"; //change to a proper name!!!!!!!!!!!!!!!!!!!
+char topicTempHum[] = "arduino-weather-exchange"; //change to a proper name!!!!!!!!!!!!!!!!!!!
 
 // defines and variable for sensor/control mode
 #define MODE_OFF    0  // not sensing light, LED off
@@ -40,48 +30,41 @@ char topicTempHum[] = "hello/world"; //change to a proper name!!!!!!!!!!!!!!!!!!
 int senseMode = 0;
 char message_buff[100];
 //setup MQTT
-  WiFiClient wifiClient;
+WiFiClient wifiClient;
+//MQTT
+PubSubClient client(serverRabbit, 1883, callback, wifiClient);
   
 void setup() {
   // initialize serial and wait for the port to open:
   Serial.begin(9600);
-  while (!Serial) ;
-
-  // check for the presence of the shield:
-  if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present");
-    // don't continue:
-    while (true);
-  }
-
-  // scan for existing networks:
-  Serial.println("Scanning available networks...");
-
-  // attempt to connect to Wifi network:
-  while ( status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network:
-    status = WiFi.begin(ssid, pass);
-
-    // wait 10 seconds for connection:
-    delay(5000);
-  }
-
-  // you're connected now, so print out the data:
-  Serial.print("You're connected to the network");
-  printCurrentNet();
-  printWifiData();
-
+  while (!Serial);
+  
+  connectToWifi();
+  
   // set the LED pins as outputs
   // the for() loop saves some extra coding
   for (int pinNumber = 2; pinNumber < 5; pinNumber++) {
     pinMode(pinNumber, OUTPUT);
     digitalWrite(pinNumber, LOW);
   }
+  connectToMQTT();
+}
 
+void loop() {
+  Serial.println("loop");
+  float tempVar = readTemperature();
+  char temp[10];
+  dtostrf(tempVar, 2, 2, temp);
+  Serial.print("DEBUG :: Temp value return is : ");
+  Serial.println(temp);
+  publishTempHumMsg(temp, "0");
   
-//  client = PubSubClient(serverRabbit, 1883, callback, wifiClient);
+  // MQTT client loop processing
+  client.loop();
+}
+
+void connectToMQTT() {
+    
 // if (!client.connected()) {
 //    delay(1000);
 //    Serial.println("Connecting to RabbitMQ");
@@ -91,105 +74,42 @@ void setup() {
 //      Serial.println("Failed to Connect to RabbitMQ");
 //    }
 // }
- 
- if (wifiClient.connect(server, 80)) {
-    Serial.println("connected");
-    // Make a HTTP request:
-    wifiClient.println("GET /search?q=arduino HTTP/1.1");
-    wifiClient.println("Host: www.google.com");
-    wifiClient.println("Connection: close");
-    wifiClient.println();
-  } 
-  else {
-    // kf you didn't get a connection to the server:
-    Serial.println("WIFI google connection failed");
-  }
-}
-
-void loop() {
-  Serial.println("loop");
-  
-  if (wifiClient.available()) {
-    char c = wifiClient.read();
-    Serial.print(c);
-  }
-  
-  float tempVar = readTemperature();
-  char temp[10];
-  dtostrf(tempVar, 2, 2, temp);
-  Serial.print("Temp value return is : ");
-  Serial.println(temp);
-//  publishTempHumMsg(temp, "0");
-}
-
-
-void publishTempHumMsg(String temp, String humidity) {
-  
-  if (!client.connected()) {
+if (!client.connected()) {
     delay(1000);
-    Serial.println("Connecting to RabbitMQ");
-    if (client.connect("ArduinoClient", user, pwd)) {
-      client.publish("topic","Connected to RabbitMQ");
-      Serial.println("Connected to RabbitMQ");
+    Serial.println("INFO :: Connecting to RabbitMQ");
+    if (client.connect("ArduinoClient", "guest", "guest")) {
+      Serial.println("DEBUG :: About to sent message......");
+      client.publish("arduino-weather-exchange","Connected to RabbitMQ");
+      Serial.println("INFO :: Connected to RabbitMQ");
     } else {
-      Serial.println("Failed to Connect to RabbitMQ");
+      Serial.println("ERROR :: Failed to Connect to RabbitMQ");
     }
   } else {
     client.publish("outTopic", "hello world");
   }
-  
-//  if (client.connect("arduinoClient", "guest", "guest")) {
-//    Serial.println("connected to rabbit!!!");
-//    client.publish("outTopic", "hello world");
+}
+
+void publishTempHumMsg(String temp, String humidity) {
 //    client.subscribe("inTopic");
-//  } else {
-//    Serial.println("Failed to connect!!! 101");
-//  }
   //MQTT send json???
   //String pubString = "{\"report\":{\"temp\": \"" + String(temperature) + "\"}}";
   //      pubString.toCharArray(message_buff, pubString.length()+1);
-  Serial.println("sending temp ");
+  Serial.println("DEBUG :: Sending temp ");
   Serial.println(temp);
   //"io.m2m/arduino/lightsensor"
-  client.publish("topicTempHum", "temp");
-
-  // MQTT client loop processing
- // client.loop();
-}
-
-void printMacAddress() {
-  // the MAC address of your Wifi shield
-  byte mac[6];
-
-  // print your MAC address:
-  WiFi.macAddress(mac);
-  Serial.print("MAC: ");
-  Serial.print(mac[5], HEX);
-  Serial.print(":");
-  Serial.print(mac[4], HEX);
-  Serial.print(":");
-  Serial.print(mac[3], HEX);
-  Serial.print(":");
-  Serial.print(mac[2], HEX);
-  Serial.print(":");
-  Serial.print(mac[1], HEX);
-  Serial.print(":");
-  Serial.println(mac[0], HEX);
+  client.publish("arduino-weather-exchange", "temp");
 }
 
 
 void printWifiData() {
-  Serial.println("------> printWifiData s");
   // print your WiFi shield's IP address:
   IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
+  Serial.print("DEBUG :: IP Address: ");
   Serial.println(ip);
-  //Serial.println(ip);
-
   // print your MAC address:
   byte mac[6];
   WiFi.macAddress(mac);
-  Serial.print("MAC address: ");
+  Serial.print("DEBUG :: MAC address: ");
   Serial.print(mac[5], HEX);
   Serial.print(":");
   Serial.print(mac[4], HEX);
@@ -201,14 +121,11 @@ void printWifiData() {
   Serial.print(mac[1], HEX);
   Serial.print(":");
   Serial.println(mac[0], HEX);
-  Serial.println("------> printWifiData e");
-
 }
 
 void printCurrentNet() {
-  Serial.println("------> printCurrentNet s");
   // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
+  Serial.print("DEBUG :: SSID: ");
   Serial.println(WiFi.SSID());
 
   // print the MAC address of the router you're attached to:
@@ -229,15 +146,14 @@ void printCurrentNet() {
 
   // print the received signal strength:
   long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
+  Serial.print("DEBUG :: Signal strength (RSSI):");
   Serial.println(rssi);
 
   // print the encryption type:
   byte encryption = WiFi.encryptionType();
-  Serial.print("Encryption Type:");
+  Serial.print("DEBUG :: Encryption Type:");
   Serial.println(encryption, HEX);
   Serial.println();
-  Serial.println("------> printCurrentNet e");
 }
 
 
@@ -247,7 +163,7 @@ float readTemperature() {
   int sensorVal = analogRead(sensorPin);
 
   // send the 10-bit sensor value out the serial port
-  Serial.print("sensor Value: ");
+  Serial.print("DEBUG :: Sensor Value: ");
   Serial.print(sensorVal);
 
   // convert the ADC reading to voltage
@@ -291,6 +207,32 @@ float readTemperature() {
   
   //TODO return the temperature value from this function
   return temperature;
+}
+
+void connectToWifi() {
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("Error :: WiFi shield not present");
+    while (true);// don't continue:
+  }
+  // scan for existing networks:
+  Serial.println("Scanning available networks...");
+
+  // attempt to connect to Wifi network:
+  while ( status != WL_CONNECTED) {
+    Serial.print("INFO :: Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+    Serial.print("DEBUG :: Status of network connection is ");
+    Serial.println(status);
+    // wait 10 seconds for connection:
+    delay(3000);
+  }
+  // you're connected now, so print out the data:
+  Serial.println("DEBUG :: You're connected to the network");
+  printCurrentNet();
+  printWifiData();
 }
 
 // handles message arrived on subscribed topic(s)
