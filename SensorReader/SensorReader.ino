d4376262ffd06b71949aba27108cc2b2d4416cad
EugeneBell <eugene_bell@yahoo.com>
2015-01-11 22:57:48 +0000

    Added DHT11 sensor support for Humidity (and temperature) and pushing data to MQTT. Also connected tilt switch sensor push to MQTT.

--- SensorReader/SensorReader.ino
+++ SensorReader/SensorReader.ino
@@ -1,3 +1,6 @@
+#include <DHT.h>
+
+
 #include <WiFi.h>
 #include <WiFiClient.h>
 //#include <WiFiServer.h>
@@ -5,8 +8,13 @@
 #include <PubSubClient.h>
 #include <SPI.h>
 
+#define DHTTYPE DHT11   // DHT 11 
+#define DHTPIN A1 
+DHT dht(DHTPIN, DHTTYPE);
+
 // named constant for the pin the sensor is connected to
 const int sensorPin = A0;
+const int tiltSensorPin = A2;
 // room temperature in Celcius
 const float baselineTemp = 30.0;
 
@@ -24,6 +32,10 @@ char clientName[] = "clientA";
 char user[] = "mqtt";
 char pwd[] = "mqtt";
 int port = 1883;
+int tiltState;
+int previousTiltState = 2;
+
+//Time on arduino http://playground.arduino.cc/Code/DateTime
 
 char topicTempHum[] = "arduino-weather-exchange"; //change to a proper name!!!!!!!!!!!!!!!!!!!
 
@@ -52,27 +64,51 @@ void setup() {
     digitalWrite(pinNumber, LOW);
   }
   connectToMQTT();
- 
+  dht.begin();
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  delay(2000);
 }
 
 void loop() {
   Serial.println("DEBUG :: loop()");
   float tempVar = readTemperature();
+  float humVar = getDHT11HumidityReadings();
   char temp[10];
+  char humidity[10];
   dtostrf(tempVar, 2, 2, temp);
+  dtostrf(humVar, 2, 2, humidity);
   Serial.print("DEBUG :: Temp value return is : ");
   Serial.println(temp);
-  publishTempHumMsg(temp, "0");
-  
+  Serial.print("DEBUG :: Humidity value return is : ");
+  Serial.println(humidity);
+  publishTempHumMsg(temp, humidity);
   // MQTT client loop processing
   client.loop();
+}
+
+void checkTiltState() {
+  tiltState = digitalRead(tiltSensorPin);
+  Serial.print("INFO :: [1 -> On, 0 -> Off] :: Tilt State is : ");
+  Serial.println(tiltState);
+  if (tiltState != previousTiltState) {
+    previousTiltState = tiltState;
+    String tiltMsg = getSensorId();
+    tiltMsg.concat(",");
+    tiltMsg.concat(tiltState);
+//    tiltMsg.concat(",");
+//    tiltMsg.concat(time);
+    char message_buff[30];
+    tiltMsg.toCharArray(message_buff, tiltMsg.length()+1);
+    //send tilt state to the tilt queue
+    client.publish("arduino-tilt-exchange", message_buff);
+  }
+  
 }
 
 void connectToMQTT() {
     Serial.println("INFO :: Connecting to RabbitMQ");
     if (client.connect(clientName, user, pwd)) {
-//      Serial.println("DEBUG :: About to sent message......");
-//      client.publish("arduino-weather-queue","Sensor1,10.0,11.3");
       Serial.println("INFO :: Connected to RabbitMQ");
     } else {
       Serial.println("ERROR :: Failed to Connect to RabbitMQ");
@@ -80,24 +116,18 @@ void connectToMQTT() {
 }
 
 void publishTempHumMsg(String temp, String humidity) {
-//    client.subscribe("inTopic");
-  //MQTT send json???
-  //String pubString = "{\"report\":{\"temp\": \"" + String(temperature) + "\"}}";
-  //      pubString.toCharArray(message_buff, pubString.length()+1);
+  //client.subscribe("inTopic");
   Serial.println("DEBUG :: Sending temp ");
   Serial.println(temp);
-  //"io.m2m/arduino/lightsensor"
-  
   //Format for the message to the following SensorId,temperature,humidity
-  
-  String tempHumMsg = getSensorId();// + "," + temp + "," + "0.01";
+  String tempHumMsg = getSensorId();
   tempHumMsg.concat(",");
   tempHumMsg.concat(temp);
   tempHumMsg.concat(",");
   tempHumMsg.concat(humidity);
   char message_buff[80];
   tempHumMsg.toCharArray(message_buff, tempHumMsg.length()+1);
-  client.publish("arduino-weather-exchange", message_buff);//"Sensor3,17.0,16.3");//getSensorId());// + "," + temp + "," + humidity);
+  client.publish("arduino-weather-exchange", message_buff);
 }
 
 String getSensorId() {
@@ -110,7 +140,7 @@ void printWifiData() {
   IPAddress ip = WiFi.localIP();
   Serial.print("DEBUG :: IP Address: ");
   Serial.println(ip);
-  // print your MAC address:
+  // print MAC address:
   byte mac[6];
   WiFi.macAddress(mac);
   Serial.print("DEBUG :: MAC address: ");
@@ -125,83 +155,83 @@ void printWifiData() {
   Serial.print(mac[1], HEX);
   Serial.print(":");
   Serial.println(mac[0], HEX);
-}
-
-void printCurrentNet() {
-  // print the SSID of the network you're attached to:
+    // print the SSID of the network you're attached to:
   Serial.print("DEBUG :: SSID: ");
   Serial.println(WiFi.SSID());
-
-  // print the MAC address of the router you're attached to:
-  byte bssid[6];
-  WiFi.BSSID(bssid);
-  Serial.print("MAC: ");
-  Serial.print(bssid[5], HEX);
-  Serial.print(":");
-  Serial.print(bssid[4], HEX);
-  Serial.print(":");
-  Serial.print(bssid[3], HEX);
-  Serial.print(":");
-  Serial.print(bssid[2], HEX);
-  Serial.print(":");
-  Serial.print(bssid[1], HEX);
-  Serial.print(":");
-  Serial.println(bssid[0], HEX);
-
   // print the received signal strength:
   long rssi = WiFi.RSSI();
   Serial.print("DEBUG :: Signal strength (RSSI):");
   Serial.println(rssi);
-
   // print the encryption type:
   byte encryption = WiFi.encryptionType();
   Serial.print("DEBUG :: Encryption Type:");
   Serial.println(encryption, HEX);
 }
 
+float getDHT11HumidityReadings() {
+  float h = dht.readHumidity();
+  // Read temperature as Celsius
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit
+  float f = dht.readTemperature(true);
+  
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("mmmmh!!! should put a retry count of 3 here !!! Failed to read from DHT sensor! looping!!");
+    getDHT11HumidityReadings();
+  }
+
+  // Compute heat index
+  // Must send in temp in Fahrenheit!
+  float hi = dht.computeHeatIndex(f, h);
+
+  Serial.print("Humidity: "); 
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: "); 
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hi);
+  Serial.println(" *F");
+  delay(500);
+ return h;
+}
 
 float readTemperature() {
-  // read the value on AnalogIn pin 0
-  // and store it in a variable
+  // read the value on AnalogIn pin 0 and store it in a variable
   int sensorVal = analogRead(sensorPin);
-
   // send the 10-bit sensor value out the serial port
   Serial.print("DEBUG :: Sensor Value: ");
   Serial.print(sensorVal);
-
   // convert the ADC reading to voltage
   float voltage = (sensorVal / 1024.0) * 5.0;
-
   // Send the voltage level out the Serial port
   Serial.print(", Volts: ");
   Serial.print(voltage);
 
-  // convert the voltage to temperature in degrees C
-  // the sensor changes 10 mV per degree
-  // the datasheet says there's a 500 mV offset
-  // ((volatge - 500mV) times 100)
+  // convert the voltage to temperature in degrees C the sensor changes 10 mV per degree
+  // the datasheet says there's a 500 mV offset ((volatge - 500mV) times 100)
   Serial.print(", degrees C: ");
   float temperature = (voltage - .5) * 100;
   Serial.println(temperature);
 
-  // if the current temperature is lower than the baseline
-  // turn off all LEDs
-  if (temperature < baselineTemp) {
+  // if the current temperature is lower than the baseline turn off all LEDs
+  if (temperature < baselineTemp) { // if the temperature rises 2-4 degrees, turn an LED on
     digitalWrite(2, LOW);
     digitalWrite(3, LOW);
     digitalWrite(4, LOW);
-  } // if the temperature rises 2-4 degrees, turn an LED on
-  else if (temperature >= baselineTemp + 2 && temperature < baselineTemp + 4) {
+  } else if (temperature >= baselineTemp + 2 && temperature < baselineTemp + 4) {
     digitalWrite(2, HIGH);
     digitalWrite(3, LOW);
     digitalWrite(4, LOW);
-  } // if the temperature rises 4-6 degrees, turn a second LED on
-  else if (temperature >= baselineTemp + 4 && temperature < baselineTemp + 6) {
+  }  else if (temperature >= baselineTemp + 4 && temperature < baselineTemp + 6) { // if the temperature rises 4-6 degrees, turn a second LED on
     digitalWrite(2, HIGH);
     digitalWrite(3, HIGH);
     digitalWrite(4, LOW);
-  } // if the temperature rises more than 6 degrees, turn all LEDs on
-  else if (temperature >= baselineTemp + 6) {
+  } else if (temperature >= baselineTemp + 6) { // if the temperature rises more than 6 degrees, turn all LEDs on
     digitalWrite(2, HIGH);
     digitalWrite(3, HIGH);
     digitalWrite(4, HIGH);
@@ -234,7 +264,6 @@ void connectToWifi() {
   }
   // you're connected now, so print out the data:
   Serial.println("DEBUG :: You're connected to the network");
-  printCurrentNet();
   printWifiData();
 }
 
